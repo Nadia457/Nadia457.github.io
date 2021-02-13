5706df1213a038e321404f410f42cf05feaab14f
Nadia457 <73061342+Nadia457@users.noreply.github.com>
2021-02-13 21:21:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const keyboard = {
+action:true,
+photo:"k_1.jpg",
+reviews:204,
+price:1399};
+
+const products=document.getElementById('products');
+
+let x=0;
+for(i=0; i<keyboard.length; i++ ){
+
+let isaction=``;
+let pint=0;
+pint +=keyboard[i].reviews/2;
+let res=max-keyboard[i].price;
+
+if(keyboard[i].action){
+	isAction=`<div class="action">Акція</div>`}
+     pint += 100;
+
+}
+ pint += res;
+
+ cards_text=
+ `
+ <div class="card">
+			<div class="header">
+				${isAction} </div>
+			
+			<div class="card_image">
+				<img src="${keyboard[i].photo}">
+			</div>
+			
+			<div class="card_info">
+				<div class="${keyboard[i].name}">
+					
+				</div>
+				
+				<div class="reviews">
+					${keyboard[i].reviews} відгуки
+				</div>
+				
+				<div class="price">
+					${keyboard[i].price}
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	`;
+	
+	
+
+	console.log(keyboard);
+	
